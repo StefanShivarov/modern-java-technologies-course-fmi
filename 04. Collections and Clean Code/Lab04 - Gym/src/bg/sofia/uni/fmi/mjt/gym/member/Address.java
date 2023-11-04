f851f08e3d7f0b804c4c11e25ca5ef8123bf63c7
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-11-04 15:09:16 +0200

    Update Address.java

--- 04. Collections and Clean Code/Lab04 - Gym/src/bg/sofia/uni/fmi/mjt/gym/member/Address.java
+++ 04. Collections and Clean Code/Lab04 - Gym/src/bg/sofia/uni/fmi/mjt/gym/member/Address.java
@@ -2,7 +2,8 @@ package bg.sofia.uni.fmi.mjt.gym.member;
 
 public record Address(double longitude, double latitude) {
 
-    public double getDistanceTo(Address other){
-        return Math.sqrt(Math.pow(other.longitude - this.longitude, 2) + Math.pow(other.latitude - this.latitude, 2));
+    public double getDistanceTo(Address other) {
+        return Math.sqrt(Math.pow(other.longitude - this.longitude, 2)
+                + Math.pow(other.latitude - this.latitude, 2));
     }
 }
