5cdeea58eb0f8a73eb0b60a6c7d1d4befc59b51b
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-11-04 16:15:17 +0200

    Create GymMemberProximityComparator.java

---
+++ 04. Collections and Clean Code/Lab04 - Gym/src/bg/sofia/uni/fmi/mjt/gym/member/comparators/GymMemberProximityComparator.java
@@ -0,0 +1,19 @@
+package bg.sofia.uni.fmi.mjt.gym.member.comparators;
+
+import bg.sofia.uni.fmi.mjt.gym.member.Address;
+import bg.sofia.uni.fmi.mjt.gym.member.GymMember;
+
+import java.util.Comparator;
+
+public class GymMemberProximityComparator implements Comparator<GymMember> {
+
+    private final Address gymAddress;
+
+    public GymMemberProximityComparator(Address gymAddress) {
+        this.gymAddress = gymAddress;
+    }
+    @Override
+    public int compare(GymMember o1, GymMember o2) {
+        return Double.compare(o2.getAddress().getDistanceTo(gymAddress), o1.getAddress().getDistanceTo(gymAddress));
+    }
+}
