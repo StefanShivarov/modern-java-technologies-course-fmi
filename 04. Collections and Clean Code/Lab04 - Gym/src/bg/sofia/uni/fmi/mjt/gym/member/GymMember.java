34b01fb3477f6de0a9bfce707b97d3b40009e18d
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-11-04 14:10:49 +0200

    Create GymMember.java

---
+++ 04. Collections and Clean Code/Lab04 - Gym/src/bg/sofia/uni/fmi/mjt/gym/member/GymMember.java
@@ -0,0 +1,32 @@
+package bg.sofia.uni.fmi.mjt.gym.member;
+
+import bg.sofia.uni.fmi.mjt.gym.workout.Exercise;
+import bg.sofia.uni.fmi.mjt.gym.workout.Workout;
+
+import java.time.DayOfWeek;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public interface GymMember {
+
+    String getName();
+
+    int getAge();
+
+    String getPersonalIdNumber();
+
+    Gender getGender();
+
+    Address getAddress();
+
+    Map<DayOfWeek, Workout> getTrainingProgram();
+
+    void setWorkout(DayOfWeek day, Workout workout);
+
+    Collection<DayOfWeek> getDaysFinishingWith(String exerciseName);
+
+    void addExercise(DayOfWeek day, Exercise exercise);
+
+    void addExercises(DayOfWeek day, List<Exercise> exercises);
+}
