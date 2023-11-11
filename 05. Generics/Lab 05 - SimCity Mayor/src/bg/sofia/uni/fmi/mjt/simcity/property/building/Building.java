ea19b2059c0853101ad2794dd70a382df3d181cf
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-11-11 13:16:56 +0200

    implement building

---
+++ 05. Generics/Lab 05 - SimCity Mayor/src/bg/sofia/uni/fmi/mjt/simcity/property/building/Building.java
@@ -0,0 +1,47 @@
+package bg.sofia.uni.fmi.mjt.simcity.property.building;
+
+import bg.sofia.uni.fmi.mjt.simcity.property.billable.Billable;
+import bg.sofia.uni.fmi.mjt.simcity.property.buildable.BuildableType;
+
+public class Building implements Billable {
+
+    private final BuildableType type;
+    private final int area;
+    private double waterConsumption;
+    private double electricityConsumption;
+    private double naturalGasConsumption;
+
+    public Building(BuildableType type, int area, double waterConsumption, double electricityConsumption, double naturalGasConsumption) {
+        this.type = type;
+        this.area = area;
+        this.waterConsumption = waterConsumption;
+        this.electricityConsumption = electricityConsumption;
+        this.naturalGasConsumption = naturalGasConsumption;
+    }
+
+    @Override
+    public double getWaterConsumption() {
+        return waterConsumption;
+    }
+
+    @Override
+    public double getElectricityConsumption() {
+        return electricityConsumption;
+    }
+
+    @Override
+    public double getNaturalGasConsumption() {
+        return naturalGasConsumption;
+    }
+
+    @Override
+    public BuildableType getType() {
+        return type;
+    }
+
+    @Override
+    public int getArea() {
+        return area;
+    }
+
+}
