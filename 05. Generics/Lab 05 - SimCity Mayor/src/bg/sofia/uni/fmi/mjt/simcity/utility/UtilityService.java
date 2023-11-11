54691b6b68181a88eaadfefe2f85370faf188501
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-11-11 14:20:45 +0200

    implement utility service

--- 05. Generics/Lab 05 - SimCity Mayor/src/bg/sofia/uni/fmi/mjt/simcity/utility/UtilityService.java
+++ 05. Generics/Lab 05 - SimCity Mayor/src/bg/sofia/uni/fmi/mjt/simcity/utility/UtilityService.java
@@ -2,11 +2,13 @@ package bg.sofia.uni.fmi.mjt.simcity.utility;
 
 import bg.sofia.uni.fmi.mjt.simcity.property.billable.Billable;
 
+import static bg.sofia.uni.fmi.mjt.simcity.helper.HelperFunctions.roundToTwoDecimalPlaces;
+
 import java.util.Map;
 
 public class UtilityService implements UtilityServiceAPI {
 
-    private Map<UtilityType, Double> taxRates;
+    private final Map<UtilityType, Double> taxRates;
 
     public UtilityService(Map<UtilityType, Double> taxRates) {
         this.taxRates = taxRates;
@@ -14,17 +16,52 @@ public class UtilityService implements UtilityServiceAPI {
 
     @Override
     public <T extends Billable> double getUtilityCosts(UtilityType utilityType, T billable) {
-        return 0;
+        if (utilityType == null || billable == null) {
+            throw new IllegalArgumentException("Invalid arguments!");
+        }
+
+        double consumption = switch (utilityType) {
+            case WATER -> billable.getWaterConsumption();
+            case ELECTRICITY -> billable.getElectricityConsumption();
+            case NATURAL_GAS -> billable.getNaturalGasConsumption();
+        };
+
+        return roundToTwoDecimalPlaces(taxRates.get(utilityType) * consumption);
     }
 
     @Override
     public <T extends Billable> double getTotalUtilityCosts(T billable) {
-        return 0;
+        if (billable == null) {
+            throw new IllegalArgumentException("Billable is null!");
+        }
+
+        double waterCost = taxRates.get(UtilityType.WATER) * billable.getWaterConsumption();
+        double electricityCost = taxRates.get(UtilityType.ELECTRICITY) * billable.getElectricityConsumption();
+        double gasCost = taxRates.get(UtilityType.NATURAL_GAS) * billable.getNaturalGasConsumption();
+
+        return roundToTwoDecimalPlaces(waterCost + electricityCost + gasCost);
     }
 
     @Override
     public <T extends Billable> Map<UtilityType, Double> computeCostsDifference(T firstBillable, T secondBillable) {
-        return null;
+        if (firstBillable == null || secondBillable == null) {
+            throw new IllegalArgumentException("Billable is null!");
+        }
+
+        double waterDiff = roundToTwoDecimalPlaces(
+                taxRates.get(UtilityType.WATER) *
+                        Math.abs(firstBillable.getWaterConsumption() - secondBillable.getWaterConsumption())
+        );
+        double electricityDiff = roundToTwoDecimalPlaces(
+                taxRates.get(UtilityType.ELECTRICITY) *
+                        Math.abs(firstBillable.getElectricityConsumption() - secondBillable.getElectricityConsumption())
+        );
+        double naturalGasDiff = roundToTwoDecimalPlaces(
+                taxRates.get(UtilityType.NATURAL_GAS) *
+                        Math.abs(firstBillable.getNaturalGasConsumption() - secondBillable.getNaturalGasConsumption())
+        );
+
+        return Map.of(UtilityType.WATER, waterDiff, UtilityType.ELECTRICITY, electricityDiff, UtilityType.NATURAL_GAS, naturalGasDiff);
     }
 
 }
