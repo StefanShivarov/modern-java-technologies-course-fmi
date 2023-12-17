616f077dedefa8cf65d1bf30bc765c972f778285
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-12-17 15:12:21 +0200

    make concurrency work properly

--- 09. Threads/Lab 09 - Retro Photo Album Maker/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
+++ 09. Threads/Lab 09 - Retro Photo Album Maker/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
@@ -1,12 +1,10 @@
 package bg.sofia.uni.fmi.mjt.photoalbum;
 
 import bg.sofia.uni.fmi.mjt.photoalbum.image.Image;
+import bg.sofia.uni.fmi.mjt.photoalbum.image.threads.ImageConsumer;
+import bg.sofia.uni.fmi.mjt.photoalbum.image.threads.ImageProducer;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,6 +12,10 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
 
@@ -29,42 +31,59 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
     public void processImages(String sourceDirectory, String outputDirectory) {
         Path sourcePath = Paths.get(sourceDirectory);
         Path outputPath = Paths.get(outputDirectory);
+        createDirectoriesIfNecessary(outputPath);
 
-        try {
-            if (!Files.exists(outputPath)) {
-                Files.createDirectories(outputPath);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        AtomicBoolean allImagesLoaded = new AtomicBoolean(false);
+        AtomicInteger loadedImagesAmount = new AtomicInteger(0);
 
         List<Thread> producerThreads = new ArrayList<>();
         try {
-            Files.walk(sourcePath)
-                    .filter(Files::isRegularFile)
-                    .filter(path -> path.toString().toLowerCase().endsWith(".jpg")
-                            || path.toString().toLowerCase().endsWith(".jpeg")
-                            || path.toString().toLowerCase().endsWith(".png"))
-                    .forEach(path -> {
-                        Thread producer = new Thread(() -> handleLoadedImage(path));
-                        producerThreads.add(producer);
-                        producer.start();
-                    });
+            List<Path> imagePaths = getImagePaths(sourcePath);
+            AtomicInteger imagesAmount = new AtomicInteger(imagePaths.size());
+            imagePaths.forEach(path -> {
+                Thread producer = new Thread(
+                        new ImageProducer(path, images, loadedImagesAmount, imagesAmount, allImagesLoaded)
+                );
+                producerThreads.add(producer);
+                producer.start();
+            });
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            throw new RuntimeException("Failed to get image paths! " + e.getMessage(), e);
         }
 
         List<Thread> consumerThreads = new ArrayList<>(imageProcessorsCount);
         for (int i = 0; i < imageProcessorsCount; i++) {
-            Thread consumer = new Thread(() -> consumeAndProcessImages(outputDirectory));
+            Thread consumer = new Thread(
+                    new ImageConsumer(outputDirectory, images, allImagesLoaded)
+            );
             consumer.setName("Consumer-" + i);
             consumerThreads.add(i, consumer);
             consumer.start();
         }
 
         joinThreads(producerThreads);
-        notifyConsumersForEndOfImages();
         joinThreads(consumerThreads);
+    }
+
+    private void createDirectoriesIfNecessary(Path outputPath) {
+        try {
+            if (!Files.exists(outputPath)) {
+                Files.createDirectories(outputPath);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error while creating directories! " + e.getMessage(), e);
+        }
+    }
+
+    public static List<Path> getImagePaths(Path sourcePath) throws IOException {
+        try (Stream<Path> paths = Files.walk(sourcePath)) {
+            return paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().toLowerCase().endsWith(".jpg")
+                            || path.toString().toLowerCase().endsWith(".jpeg")
+                            || path.toString().toLowerCase().endsWith(".png"))
+                    .collect(Collectors.toList());
+        }
     }
 
     private void joinThreads(List<Thread> threads) {
@@ -74,90 +93,9 @@ public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
+
+            System.out.println(thread.getName() + " joined!");
         }
-    }
-
-    private void notifyConsumersForEndOfImages() {
-        synchronized (images) {
-            for (int i = 0; i < imageProcessorsCount; i++) {
-                images.add(new Image("", null));
-            }
-            images.notifyAll();
-        }
-    }
-
-    private void consumeAndProcessImages(String outputDirectory) {
-        while (true) {
-            Image image;
-            synchronized (images) {
-                while (images.isEmpty()) {
-                    try {
-                        images.wait();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        return;
-                    }
-                }
-                image = images.poll();
-                images.notifyAll();
-            }
-
-            if (image.getName().isEmpty()) {
-                break;
-            }
-
-            saveConvertedImage(convertToBlackAndWhite(image), outputDirectory);
-        }
-    }
-
-    private void handleLoadedImage(Path imagePath) {
-        synchronized (images) {
-            images.add(loadImage(imagePath));
-            images.notifyAll();
-        }
-    }
-
-    public Image loadImage(Path imagePath) {
-        try {
-            BufferedImage imageData = ImageIO.read(imagePath.toFile());
-            return new Image(imagePath.getFileName().toString(), imageData);
-        } catch (IOException e) {
-            throw new UncheckedIOException(String.format("Failed to load image %s", imagePath.toString()), e);
-        }
-    }
-
-    private Image convertToBlackAndWhite(Image image) {
-        BufferedImage processedData = new BufferedImage(image.getData().getWidth(), image.getData().getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-        processedData.getGraphics().drawImage(image.getData(), 0, 0, null);
-
-        return new Image(image.getName(), processedData);
-    }
-
-    private void saveConvertedImage(Image convertedImage, String outputDirectory) {
-        File outputDirectoryFile = new File(outputDirectory);
-
-        if (!outputDirectoryFile.exists()) {
-            outputDirectoryFile.mkdirs();
-        }
-
-        File outputFile = new File(outputDirectory, convertedImage.getName());
-        String imageFormat = getImageFormat(convertedImage.getName());
-
-        try {
-            ImageIO.write(convertedImage.getData(), imageFormat, outputFile);
-        } catch (IOException e) {
-            System.out.println("Failed to save image: " + e.getMessage());
-        }
-    }
-
-    private String getImageFormat(String imageName) {
-        int lastDotIndex = imageName.lastIndexOf('.');
-
-        if (lastDotIndex != -1 && lastDotIndex < imageName.length() - 1) {
-            return imageName.substring(lastDotIndex + 1).toLowerCase();
-        }
-
-        return "png";
     }
 
 }
