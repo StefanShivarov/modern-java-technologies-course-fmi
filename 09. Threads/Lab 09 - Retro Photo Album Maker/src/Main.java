444fe8c9b4d5946b46da9eaba0559edec15c222b
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-12-17 15:30:56 +0200

    Update Main.java

--- 09. Threads/Lab 09 - Retro Photo Album Maker/src/Main.java
+++ 09. Threads/Lab 09 - Retro Photo Album Maker/src/Main.java
@@ -7,7 +7,19 @@ public class Main {
         MonochromeAlbumCreator albumCreator = new ParallelMonochromeAlbumCreator(5);
         long startTime = System.currentTimeMillis();
         albumCreator.processImages("resources/original", "resources/converted");
-        System.out.println("Execution time: " + (System.currentTimeMillis() - startTime));
+
+        /*
+           Sleeping the thread for 1000 milliseconds to make sure it prints out the execution time
+           after the method has finished completely
+         */
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Execution time: " + (System.currentTimeMillis() - startTime - 1000));
     }
 
 }
