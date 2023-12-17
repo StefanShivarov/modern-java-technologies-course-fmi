87b799b1e690617eb27e87615b16d07e35ee23f5
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-12-18 01:29:17 +0200

    Update Mission.java

--- Homeworks/Homework 2/Space Scanner/src/bg/sofia/uni/fmi/mjt/space/mission/Mission.java
+++ Homeworks/Homework 2/Space Scanner/src/bg/sofia/uni/fmi/mjt/space/mission/Mission.java
@@ -5,14 +5,15 @@ import bg.sofia.uni.fmi.mjt.space.rocket.RocketStatus;
 import java.time.LocalDate;
 import java.util.Optional;
 
-public record Mission(String id,
-                      String company,
-                      String location,
-                      LocalDate date,
-                      Detail detail,
-                      RocketStatus rocketStatus,
-                      Optional<Double> cost,
-                      MissionStatus missionStatus
+public record Mission(
+        String id,
+        String company,
+        String location,
+        LocalDate date,
+        Detail detail,
+        RocketStatus rocketStatus,
+        Optional<Double> cost,
+        MissionStatus missionStatus
 ) {
 
 
