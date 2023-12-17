7563995982aeac4f637b1c62df9404002627716a
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-12-17 15:34:02 +0200

    Update ParallelMonochromeAlbumCreator.java

--- 09. Threads/Lab 09 - Retro Photo Album Maker/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
+++ 09. Threads/Lab 09 - Retro Photo Album Maker/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
@@ -9,7 +9,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.atomic.AtomicBoolean;
