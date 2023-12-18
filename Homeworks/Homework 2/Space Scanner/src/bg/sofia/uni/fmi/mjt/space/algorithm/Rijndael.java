5083154d0fc884214dec6654dff642dc06a0c8e7
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-12-18 15:56:57 +0200

    Update Rijndael.java

--- Homeworks/Homework 2/Space Scanner/src/bg/sofia/uni/fmi/mjt/space/algorithm/Rijndael.java
+++ Homeworks/Homework 2/Space Scanner/src/bg/sofia/uni/fmi/mjt/space/algorithm/Rijndael.java
@@ -2,24 +2,62 @@ package bg.sofia.uni.fmi.mjt.space.algorithm;
 
 import bg.sofia.uni.fmi.mjt.space.exception.CipherException;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class Rijndael implements SymmetricBlockCipher {
 
-    //TODO: implement
-    public Rijndael(SecretKey secretKey) {
+    private static final String AES_ENCRYPTION_ALGORITHM = "AES";
+    private static final int BUFFER_LENGTH = 1024;
+    private final SecretKey secretKey;
 
+    public Rijndael(SecretKey secretKey) {
+        this.secretKey = secretKey;
     }
 
     @Override
     public void encrypt(InputStream inputStream, OutputStream outputStream) throws CipherException {
+        try {
+            Cipher cipher = Cipher.getInstance(AES_ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
+            try (CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, cipher)) {
+                processStreamData(inputStream, cipherOutputStream);
+            }
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+            throw new CipherException("Error occurred while encrypting data!", e);
+        }
     }
 
     @Override
     public void decrypt(InputStream inputStream, OutputStream outputStream) throws CipherException {
+        try {
+            Cipher cipher = Cipher.getInstance(AES_ENCRYPTION_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
+            try (CipherInputStream cipherInputStream = new CipherInputStream(inputStream, cipher)) {
+                processStreamData(cipherInputStream, outputStream);
+            }
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+            throw new CipherException("Error occurred while decrypting data!", e);
+        }
     }
+
+    private void processStreamData(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] buffer = new byte[BUFFER_LENGTH];
+        int bytesRead;
+
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+    }
+
 }
