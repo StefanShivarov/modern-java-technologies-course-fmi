fc7b9e93234cd99798c8a47b9a97b45b86b4dae6
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-12-26 15:24:39 +0200

    Update MJTSpaceScanner.java

--- Homeworks/Homework 2/Space Scanner/src/bg/sofia/uni/fmi/mjt/space/MJTSpaceScanner.java
+++ Homeworks/Homework 2/Space Scanner/src/bg/sofia/uni/fmi/mjt/space/MJTSpaceScanner.java
@@ -1,79 +1,268 @@
 package bg.sofia.uni.fmi.mjt.space;
 
+import bg.sofia.uni.fmi.mjt.space.algorithm.Rijndael;
 import bg.sofia.uni.fmi.mjt.space.exception.CipherException;
+import bg.sofia.uni.fmi.mjt.space.exception.TimeFrameMismatchException;
+import bg.sofia.uni.fmi.mjt.space.io.CSVReader;
+import bg.sofia.uni.fmi.mjt.space.io.parser.MissionParser;
+import bg.sofia.uni.fmi.mjt.space.io.parser.RocketParser;
 import bg.sofia.uni.fmi.mjt.space.mission.Mission;
 import bg.sofia.uni.fmi.mjt.space.mission.MissionStatus;
 import bg.sofia.uni.fmi.mjt.space.rocket.Rocket;
 import bg.sofia.uni.fmi.mjt.space.rocket.RocketStatus;
 
+import javax.crypto.SecretKey;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MJTSpaceScanner implements SpaceScannerAPI {
 
-    //TODO: implement
+    private final List<Mission> missions;
+    private final List<Rocket> rockets;
+    private final Rijndael rijndael;
+
+    public MJTSpaceScanner(Reader missionsReader, Reader rocketsReader, SecretKey secretKey) {
+        this.missions = loadMissionsFromCSVFile(missionsReader);
+        this.rockets = loadRocketsFromCSVFile(rocketsReader);
+        this.rijndael = new Rijndael(secretKey);
+    }
+
     @Override
     public Collection<Mission> getAllMissions() {
-        return null;
+        return missions;
     }
 
     @Override
     public Collection<Mission> getAllMissions(MissionStatus missionStatus) {
-        return null;
+        return missions.stream()
+                .filter(mission -> mission.missionStatus() == missionStatus)
+                .toList();
     }
 
     @Override
     public String getCompanyWithMostSuccessfulMissions(LocalDate from, LocalDate to) {
-        return null;
+        if (from.isAfter(to)) {
+            throw new TimeFrameMismatchException("Invalid time frame!");
+        }
+
+        return missions.stream()
+                .filter(mission -> mission.date().isAfter(from) && mission.date().isBefore(to))
+                .filter(mission -> mission.missionStatus() == MissionStatus.SUCCESS)
+                .collect(Collectors.groupingBy(Mission::company, Collectors.counting()))
+                .entrySet().stream()
+                .max(Comparator.comparingLong(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElse("");
     }
 
     @Override
     public Map<String, Collection<Mission>> getMissionsPerCountry() {
-        return null;
+        return missions.stream()
+                .collect(Collectors.groupingBy(
+                        mission -> mission.location().split("\\s+,\\s+")[2],
+                        Collectors.toCollection(ArrayList::new)
+                ));
     }
 
     @Override
     public List<Mission> getTopNLeastExpensiveMissions(int n, MissionStatus missionStatus, RocketStatus rocketStatus) {
-        return null;
+        return missions.stream()
+                .filter(mission -> mission.missionStatus() == missionStatus)
+                .filter(mission -> mission.rocketStatus() == rocketStatus)
+                .sorted(Comparator.comparingDouble(m -> m.cost().orElse(Double.MAX_VALUE)))
+                .limit(n)
+                .toList();
     }
 
     @Override
     public Map<String, String> getMostDesiredLocationForMissionsPerCompany() {
-        return null;
+        return missions.stream()
+                .collect(Collectors.groupingBy(
+                        Mission::company,
+                        Collectors.groupingBy(
+                                Mission::location,
+                                Collectors.counting()
+                        )
+                ))
+                .entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().entrySet().stream()
+                                .max(Map.Entry.comparingByValue())
+                                .map(Map.Entry::getKey)
+                                .orElse("Unknown")
+                ));
     }
 
     @Override
     public Map<String, String> getLocationWithMostSuccessfulMissionsPerCompany(LocalDate from, LocalDate to) {
-        return null;
+        if (from.isAfter(to)) {
+            throw new TimeFrameMismatchException("Invalid time frame!");
+        }
+
+        return missions.stream()
+                .filter(mission -> mission.date().isAfter(from) && mission.date().isBefore(to))
+                .filter(mission -> mission.missionStatus() == MissionStatus.SUCCESS)
+                .collect(Collectors.groupingBy(
+                        Mission::company,
+                        Collectors.groupingBy(
+                                Mission::location,
+                                Collectors.counting()
+                        )
+                ))
+                .entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().entrySet().stream()
+                                .max(Map.Entry.comparingByValue())
+                                .map(Map.Entry::getKey)
+                                .orElse("Unknown")
+                ));
     }
 
     @Override
     public Collection<Rocket> getAllRockets() {
-        return null;
+        return rockets;
     }
 
     @Override
     public List<Rocket> getTopNTallestRockets(int n) {
-        return null;
+        return rockets.stream()
+                .sorted(Comparator.comparingDouble(r -> r.height().orElse(0.0)))
+                .limit(n)
+                .toList();
     }
 
     @Override
     public Map<String, Optional<String>> getWikiPageForRocket() {
-        return null;
+        return rockets.stream()
+                .collect(Collectors.groupingBy(
+                        Rocket::name,
+                        Collectors.mapping(
+                                rocket -> rocket.wiki().orElse(null),
+                                Collectors.reducing((s1, s2) -> s1)
+                        )
+                ));
     }
 
     @Override
     public List<String> getWikiPagesForRocketsUsedInMostExpensiveMissions(int n, MissionStatus missionStatus, RocketStatus rocketStatus) {
-        return null;
+        List<String> mostExpensiveMissionRocketNames = missions.stream()
+                .filter(mission -> mission.missionStatus() == missionStatus)
+                .filter(mission -> mission.rocketStatus() == rocketStatus)
+                .filter(mission -> mission.cost().isPresent())
+                .sorted(Comparator.comparingDouble(mission -> mission.cost().get()))
+                .limit(n)
+                .map(mission -> mission.detail().rocketName())
+                .toList();
+
+        return mostExpensiveMissionRocketNames.stream()
+                .map(rocketName -> rockets.stream()
+                        .filter(rocket -> rocket.name().equals(rocketName))
+                        .findFirst()
+                        .flatMap(Rocket::wiki)
+                        .orElse(null)
+                )
+                .filter(Objects::nonNull)
+                .toList();
     }
 
     @Override
     public void saveMostReliableRocket(OutputStream outputStream, LocalDate from, LocalDate to) throws CipherException {
+        if (from.isAfter(to)) {
+            throw new TimeFrameMismatchException("Invalid time frame!");
+        }
 
+        String mostReliableRocketName = missions.stream()
+                .filter(mission -> mission.date().isAfter(from) && mission.date().isBefore(to))
+                .map(mission -> mission.detail().rocketName())
+                .max(Comparator.comparingDouble(this::calculateRocketReliability))
+                .orElse(null);
+
+        Rocket mostReliableRocket = rockets.stream()
+                .filter(rocket -> rocket.name().equals(mostReliableRocketName))
+                .findFirst()
+                .orElse(null);
+
+        if (mostReliableRocket != null) {
+            serializeEncryptedRocket(mostReliableRocket, outputStream);
+        }
+    }
+
+    private void serializeEncryptedRocket(Rocket rocket, OutputStream outputStream) throws CipherException {
+        try (
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)
+        ) {
+            objectOutputStream.writeObject(rocket);
+            objectOutputStream.flush();
+
+            rijndael.encrypt(
+                    new ByteArrayInputStream(byteArrayOutputStream.toByteArray()),
+                    outputStream
+            );
+        } catch (IOException e) {
+            throw new CipherException("Error occurred while encrypting and saving rocket data!", e);
+        }
+    }
+
+    private long getSuccessfulMissionsAmountForRocket(String rocketName) {
+        return missions.stream()
+                .filter(mission -> mission.detail().rocketName().equals(rocketName))
+                .filter(mission -> mission.missionStatus() == MissionStatus.SUCCESS)
+                .count();
+    }
+
+    private long getUnsuccessfulMissionsAmountForRocket(String rocketName) {
+        return missions.stream()
+                .filter(mission -> mission.detail().rocketName().equals(rocketName))
+                .filter(mission -> mission.missionStatus() == MissionStatus.FAILURE
+                        || mission.missionStatus() == MissionStatus.PARTIAL_FAILURE
+                        || mission.missionStatus() == MissionStatus.PRELAUNCH_FAILURE
+                )
+                .count();
+    }
+
+    private double calculateRocketReliability(String rocketName) {
+        long successfulMissions = getSuccessfulMissionsAmountForRocket(rocketName);
+        long unsuccessfulMissions = getUnsuccessfulMissionsAmountForRocket(rocketName);
+
+        return (2 * successfulMissions + unsuccessfulMissions) / (2.0 * (successfulMissions + unsuccessfulMissions));
+    }
+
+    private List<Mission> loadMissionsFromCSVFile(Reader missionsReader) {
+        try (CSVReader csvReader = new CSVReader(missionsReader)) {
+            return csvReader.readAllLines().stream()
+                    .skip(1)
+                    .map(MissionParser::parseFromInput)
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException("Error while parsing the missions data from the CSV file!", e);
+        }
+    }
+
+    private List<Rocket> loadRocketsFromCSVFile(Reader rocketsReader) {
+        try (CSVReader csvReader = new CSVReader(rocketsReader)) {
+            return csvReader.readAllLines().stream()
+                    .skip(1)
+                    .map(RocketParser::parseFromInput)
+                    .toList();
+        } catch (IOException e) {
+            throw new RuntimeException("Error while parsing the missions data from the CSV file!", e);
+        }
     }
 
 }
