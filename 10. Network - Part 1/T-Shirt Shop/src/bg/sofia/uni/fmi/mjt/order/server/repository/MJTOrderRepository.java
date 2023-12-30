03dab996424df9927089d4070b16c50418b8165d
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-12-30 19:17:28 +0200

    Update MJTOrderRepository.java

--- 10. Network - Part 1/T-Shirt Shop/src/bg/sofia/uni/fmi/mjt/order/server/repository/MJTOrderRepository.java
+++ 10. Network - Part 1/T-Shirt Shop/src/bg/sofia/uni/fmi/mjt/order/server/repository/MJTOrderRepository.java
@@ -1,14 +1,19 @@
 package bg.sofia.uni.fmi.mjt.order.server.repository;
 
 import bg.sofia.uni.fmi.mjt.order.server.Response;
+import bg.sofia.uni.fmi.mjt.order.server.destination.Destination;
 import bg.sofia.uni.fmi.mjt.order.server.order.Order;
+import bg.sofia.uni.fmi.mjt.order.server.tshirt.Color;
+import bg.sofia.uni.fmi.mjt.order.server.tshirt.Size;
+import bg.sofia.uni.fmi.mjt.order.server.tshirt.TShirt;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MJTOrderRepository implements OrderRepository {
 
+    private static final String INVALID_MESSAGE_PREFIX = "invalid: ";
     private static final int INVALID_ORDER_ID = -1;
     private static final int INITIAL_ID_VALUE = 1;
     private int currentOrderId;
@@ -21,12 +26,30 @@ public class MJTOrderRepository implements OrderRepository {
 
     @Override
     public Response request(String size, String color, String destination) {
-        //TODO: implement
-        return null;
+        if (size == null || color == null || destination == null) {
+            throw new IllegalArgumentException("Order request should not contain null elements!");
+        }
+
+        StringBuilder errorMessage = new StringBuilder(INVALID_MESSAGE_PREFIX);
+        TShirt tShirt = new TShirt(Size.fromString(size), Color.fromString(color));
+        Destination destinationEnum = Destination.fromString(destination);
+
+        if (!validateEnums(tShirt.size(), tShirt.color(),
+                destinationEnum, errorMessage).get()) {
+            orders.add(new Order(INVALID_ORDER_ID, tShirt, destinationEnum));
+            return Response.decline(errorMessage.toString());
+        }
+
+        orders.add(new Order(currentOrderId, tShirt, destinationEnum));
+        return Response.create(currentOrderId++);
     }
 
     @Override
     public Response getOrderById(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id can't be a non-positive number!");
+        }
+
         return orders.stream()
                 .filter(o -> o.id() == id)
                 .findFirst()
@@ -46,6 +69,37 @@ public class MJTOrderRepository implements OrderRepository {
                         .filter(o -> o.id() != INVALID_ORDER_ID)
                         .toList()
         );
+    }
+
+    private AtomicBoolean validateEnums(
+            Size size,
+            Color color,
+            Destination destination,
+            StringBuilder errorMessage
+    ) {
+        AtomicBoolean areValid = new AtomicBoolean(true);
+        if (size == Size.UNKNOWN) {
+            areValid.set(false);
+            errorMessage.append("size");
+        }
+
+        if (color == Color.UNKNOWN) {
+            if (!areValid.get()) {
+                errorMessage.append(",");
+            }
+            errorMessage.append("color");
+            areValid.set(false);
+        }
+
+        if (destination == Destination.UNKNOWN) {
+            if (!areValid.get()) {
+                errorMessage.append(",");
+            }
+            errorMessage.append("destination");
+            areValid.set(false);
+        }
+
+        return areValid;
     }
 
 }
