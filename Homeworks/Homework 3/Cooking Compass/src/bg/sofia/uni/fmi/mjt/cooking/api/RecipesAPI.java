28bf4d99e45a2bcd9af9535380e0a19b4f7c82d9
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-01-19 16:25:00 +0200

    init api client

---
+++ Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/api/RecipesAPI.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.cooking.api;
+
+public interface RecipesAPI {
+
+
+
+}
---
+++ Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/api/RecipesAPIClient.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.cooking.api;
+
+public class RecipesAPIClient implements RecipesAPI {
+}
