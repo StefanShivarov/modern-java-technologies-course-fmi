754ff5818f94a094b6d6d811df295d464eb65f03
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-01-19 19:33:52 +0200

    validation for missing required keywords

---
+++ Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/exception/RequiredKeywordsQueryException.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.mjt.cooking.exception;
+
+public class RequiredKeywordsQueryException extends Exception {
+
+    public RequiredKeywordsQueryException(String message) {
+        super(message);
+    }
+
+    public RequiredKeywordsQueryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
