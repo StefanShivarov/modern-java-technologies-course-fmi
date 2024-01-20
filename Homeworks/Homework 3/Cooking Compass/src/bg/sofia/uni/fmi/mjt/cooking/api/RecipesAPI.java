9721bb88941c078d6413291e7cb40250191cedf5
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-01-20 20:29:21 +0200

    Update RecipesAPI.java

--- Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/api/RecipesAPI.java
+++ Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/api/RecipesAPI.java
@@ -1,7 +1,18 @@
 package bg.sofia.uni.fmi.mjt.cooking.api;
 
+import bg.sofia.uni.fmi.mjt.cooking.exception.InvalidUriException;
+import bg.sofia.uni.fmi.mjt.cooking.exception.RequestFailedException;
+import bg.sofia.uni.fmi.mjt.cooking.exception.RequiredQueryMissingException;
+import bg.sofia.uni.fmi.mjt.cooking.response.Recipe;
+
+import java.util.Collection;
+import java.util.Set;
+
 public interface RecipesAPI {
 
+    Collection<Recipe> getRecipes(String keywords, Set<String> health, Set<String> mealTypes)
+            throws RequiredQueryMissingException, InvalidUriException, RequestFailedException;
 
+    String convertRecipesToJson(Collection<Recipe> recipes);
 
 }
