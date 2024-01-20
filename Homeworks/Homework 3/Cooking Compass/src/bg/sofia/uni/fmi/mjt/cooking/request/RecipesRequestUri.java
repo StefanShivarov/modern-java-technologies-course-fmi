d38fd35669283236ec223b8e61023d7714ae6af9
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-01-20 20:27:41 +0200

    add required fields to query

--- Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/request/RecipesRequestUri.java
+++ Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/request/RecipesRequestUri.java
@@ -1,17 +1,24 @@
 package bg.sofia.uni.fmi.mjt.cooking.request;
 
-import java.util.List;
+import bg.sofia.uni.fmi.mjt.cooking.enums.RequiredField;
+import bg.sofia.uni.fmi.mjt.cooking.exception.InvalidUriException;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Set;
 
 public class RecipesRequestUri {
 
-    private static final String API_ENDPOINT = "https://api.edamam.com/api/recipes/v2?type=public";
-    private static final String APP_ID = System.getenv("EDAMAM_APP_ID");
-    private static final String APP_KEY = System.getenv("EDAMAM_APP_KEY");
-    private static final String APP_ID_QUERY = "&app_id=" + APP_ID;
-    private static final String APP_KEY_QUERY = "&app_key=" + APP_KEY;
+    private static final String API_SCHEME = "https";
+    private static final String API_HOST = "api.edamam.com";
+    private static final String API_PATH = "/api/recipes/v2";
+    private static final String TYPE_QUERY = "type=public";
+    private static final String APP_ID_QUERY = "&app_id=" + System.getenv("EDAMAM_APP_ID");
+    private static final String APP_KEY_QUERY = "&app_key=" + System.getenv("EDAMAM_APP_KEY");
     private static final String MEAL_TYPE_QUERY_PREFIX = "&mealType=";
     private static final String HEALTH_QUERY_PREFIX = "&health=";
     private static final String KEYWORDS_QUERY_PREFIX = "&q=";
+    private static final String FIELDS_QUERY_PREFIX = "&field=";
     private final String keywordsQuery;
     private final String mealTypesQuery;
     private final String healthQuery;
@@ -22,18 +29,42 @@ public class RecipesRequestUri {
         this.healthQuery = builder.healthQuery;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder(API_ENDPOINT + APP_ID_QUERY + APP_KEY_QUERY);
-        result.append(KEYWORDS_QUERY_PREFIX).append(keywordsQuery);
-        if (mealTypesQuery != null && !mealTypesQuery.isBlank()) {
-            result.append(MEAL_TYPE_QUERY_PREFIX).append(mealTypesQuery);
+    public URI getUri() throws InvalidUriException {
+        try {
+            return new URI(API_SCHEME, API_HOST, API_PATH, getQuery(), null);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new InvalidUriException("Uri is invalid!");
         }
+    }
+
+    public String getQuery() {
+        StringBuilder result = new StringBuilder()
+                .append(TYPE_QUERY)
+                .append(APP_ID_QUERY)
+                .append(APP_KEY_QUERY);
+
+        if (keywordsQuery != null && !keywordsQuery.isBlank()) {
+            result.append(KEYWORDS_QUERY_PREFIX).append(keywordsQuery);
+        }
+
         if (healthQuery != null && !healthQuery.isBlank()) {
             result.append(HEALTH_QUERY_PREFIX).append(healthQuery);
         }
 
+        if (mealTypesQuery != null && !mealTypesQuery.isBlank()) {
+            result.append(MEAL_TYPE_QUERY_PREFIX).append(mealTypesQuery);
+        }
+
+        appendRequiredFieldsToQuery(result);
+
         return result.toString();
+    }
+
+    private void appendRequiredFieldsToQuery(StringBuilder sb){
+        Arrays.stream(RequiredField.values())
+                .map(RequiredField::getValue)
+                .forEach(field -> sb.append(FIELDS_QUERY_PREFIX).append(field));
     }
 
     public static RecipesRequestUriBuilder builder() {
@@ -49,17 +80,23 @@ public class RecipesRequestUri {
         private RecipesRequestUriBuilder() {}
 
         public RecipesRequestUriBuilder keywords(String keywordsQuery) {
-            this.keywordsQuery = keywordsQuery;
+            if (keywordsQuery != null) {
+                this.keywordsQuery = keywordsQuery;
+            }
             return this;
         }
 
-        public RecipesRequestUriBuilder mealTypes(List<String> mealTypes) {
-            this.mealTypesQuery = String.join(MEAL_TYPE_QUERY_PREFIX, mealTypes);
+        public RecipesRequestUriBuilder mealTypes(Set<String> mealTypes) {
+            if (mealTypes != null) {
+                this.mealTypesQuery = String.join(MEAL_TYPE_QUERY_PREFIX, mealTypes);
+            }
             return this;
         }
 
-        public RecipesRequestUriBuilder health(List<String> health) {
-            this.healthQuery = String.join(HEALTH_QUERY_PREFIX, health);
+        public RecipesRequestUriBuilder health(Set<String> health) {
+            if (health != null) {
+                this.healthQuery = String.join(HEALTH_QUERY_PREFIX, health);
+            }
             return this;
         }
 
