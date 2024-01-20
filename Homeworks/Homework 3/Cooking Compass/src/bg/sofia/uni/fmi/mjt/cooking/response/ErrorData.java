cf4c7a431f6a69991972ddcc8fbc2c40c201bba7
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-01-20 21:52:34 +0200

    add error responses

---
+++ Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/response/ErrorData.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.cooking.response;
+
+public record ErrorData(String error, String property) {
+}
---
+++ Homeworks/Homework 3/Cooking Compass/src/bg/sofia/uni/fmi/mjt/cooking/response/ErrorsResponse.java
@@ -0,0 +1,6 @@
+package bg.sofia.uni.fmi.mjt.cooking.response;
+
+import java.util.List;
+
+public record ErrorsResponse(List<ErrorData> errors) {
+}
